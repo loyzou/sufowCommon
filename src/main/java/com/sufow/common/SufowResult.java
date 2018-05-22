2ba7d3761700a1aedc802193d80bb601634c13ff
sufow <git@github.com:zoushuquan>
2018-05-22 17:35:06 +0800

    Signed-off-by: sufow <git@github.com:zoushuquan>

---
+++ src/main/java/com/sufow/common/SufowResult.java
@@ -0,0 +1,19 @@
+package com.sufow.common;
+
+public class SufowResult {
+
+	private boolean success = true;
+	private String message;
+	public boolean isSuccess() {
+		return success;
+	}
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
